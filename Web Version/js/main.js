094301a0d4af933bbda83df6d0816efe90616151
Audrey Main <meilong.main@gmail.com>
2024-11-01 13:20:39 -0400

    started adding dragging (NOT WORKING)

--- Web Version/js/main.js
+++ Web Version/js/main.js
@@ -95,6 +95,8 @@ const blockShapes = [
 ];
 //#endregion
 
+let gameGrid = new PIXI.Container();
+
 class Cell extends PIXI.Graphics {
     constructor(x = 0, y = 0, fillColor = '#F08080', borderColor = '#F4478E', size = CELLSIZE) {
         super();
@@ -121,6 +123,48 @@ class Block extends PIXI.Graphics {
 
         this.currentForm = currentForm; // the current shape after being rotated 90Deg currentForm-1 times
         if (this.currentForm < 1) this.currentForm = 1;
+
+        this.dragging = false;
+
+        this.eventmode='static';
+        this.eventmode='static';
+        this.enableDragging();
+    }
+
+    onDragStart(e) {
+        this.data = e.data;
+        this.dragging = true;
+        this.alpha = 0.5;
+    }
+
+    onDragEnd(e) {
+        this.dragging = false;
+        this.alpha = 1;
+        this.data = null;
+    }
+
+    onDragMove(e) {
+        if (this.dragging) {
+            let newPos = this.data.getLocalPosition(this.parent);
+            this.x = newPos.x;
+            this.y = newPos.y;
+        }
+    }
+
+    enableDragging() {
+        this.interactive = true;
+        this.on('pointerdown', this.onDragStart)
+        .on('pointerup', this.onDragEnd)
+        .on('pointerupoutside', this.onDragEnd)
+        .on('pointermove', this.onDragMove);
+    }
+
+    disableDragging() {
+        this.interactive = true;
+        this.off('pointerdown', this.onDragStart)
+        .off('pointerup', this.onDragEnd)
+        .off('pointerupoutside', this.onDragEnd)
+        .off('pointermove', this.onDragMove);
     }
 
     drawBlock() {
@@ -136,6 +180,10 @@ class Block extends PIXI.Graphics {
     }
 }
 
+
+
+
+
 let testCell = new Cell(36, 174, '#B0DB43', '#12EAEA', 74);
 app.stage.addChild(testCell);
 
@@ -146,6 +194,6 @@ console.log(testBlock.shape);
 console.log(rotateBlock(testBlock, 1));
 
 let testerBlock = new Block(80, 90, testBlock.shape, 4);
-testerBlock.x=160;
-testerBlock.y=280;
+testerBlock.x = 160;
+testerBlock.y = 280;
 testerBlock.drawBlock();
